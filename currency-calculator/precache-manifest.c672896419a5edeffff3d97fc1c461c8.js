9c29580ae2743fd52ab0a254a4284f483bb51b68
Alan Polar <63665110+alangiovannipolardev@users.noreply.github.com>
2020-07-05 07:54:27 -0500

    Adding currency-calculator site (#1)

---
+++ currency-calculator/precache-manifest.c672896419a5edeffff3d97fc1c461c8.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4d6781b87be3afe29f4bf1a7a90e42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c89447c2ff67cffb754a",
+    "url": "/static/css/2.492225a0.chunk.css"
+  },
+  {
+    "revision": "9c8bd9c260160d3d7fbe",
+    "url": "/static/css/main.53a453a0.chunk.css"
+  },
+  {
+    "revision": "c89447c2ff67cffb754a",
+    "url": "/static/js/2.042e1537.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.042e1537.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8bd9c260160d3d7fbe",
+    "url": "/static/js/main.75f598a9.chunk.js"
+  },
+  {
+    "revision": "a753e852908c530b7b55",
+    "url": "/static/js/runtime-main.86bb2602.js"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "778b1f251bea7412048da95b87bf816f",
+    "url": "/static/media/fa-brands-400.778b1f25.svg"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "66578cdbb6dc01f527a53971051b3e85",
+    "url": "/static/media/fa-regular-400.66578cdb.svg"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "486853107489520b3265b19b191626f8",
+    "url": "/static/media/fa-solid-900.48685310.svg"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/static/media/fa-solid-900.f3a7d3b5.woff"
+  }
+]);
